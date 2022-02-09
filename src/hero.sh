a504187e39e71f501878329e8fa17594e13df6fd
Shinichi Okada <okada.shin@gmail.com>
2022-02-09 14:45:17 +0900

    Working on writing to index.txt

--- src/hero.sh
+++ src/hero.sh
@@ -32,18 +32,20 @@ fn_hero() {
     
     #  modify file names
     bannerColor 'Renaming all files in outline dir.' "blue" "*"
-    # in heroicons/outline and heroicons/solid rename file names 
+    # in heroicons/outline rename file names 
     rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/Icon.svelte/' -- *.svg  
     bannerColor 'Renaming is done.' "green" "*"
 
     # For each svelte file modify contents of all file by adding
     bannerColor 'Modifying all files.' "blue" "*"
 
+    # VIEWBOX
     # viewBox="0 0 24 24" to {viewBox} for outline
     sed -i 's/viewBox="0 0 24 24"/{viewBox}/' ./*.*
 
-    # Insert script tag at the beginning for solid and insert class={className} and viewBox
+    # Insert script tag at the beginning and insert class={className} and viewBox
     sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 24 24";<\/script>/' ./*.* && sed -i 's/fill/class={className} &/' ./*.*
+    # END OF VIEWBOX
 
     bannerColor 'Modification is done in outline dir.' "green" "*"
 
@@ -60,12 +62,69 @@ fn_hero() {
     # Add , after each line in names.txt
     sed -i 's/$/,/' names.txt
 
-    # Create import section in index-outline.txt and index-solid.txt files.
+    # Create import section in index2 files.
     # for outline
     sed "s:\(.*\)\.svelte:import \1 from './heroicons/outline/&':" index2 > index3
     bannerColor 'Created index.js file with import.' "green" "*"
+
+    #################
+    #    INDEX.JS   #
+    #################
+    
+    bannerColor 'Adding export to index.js file.' "blue" "*"
+    # Add export{} section
+    # 1 insert export { to index.js, 
+    # 2 insert icon-names to index.js after export { 
+    # 3. append }
+    echo 'export {' >> index3 && cat index3 names.txt > index.js && echo '}' >> index.js
+
+    rm names.txt index2 index3
+
+    bannerColor 'Added export to index.js file.' "green" "*"
+
+    
+    ######################### 
+    #         SOLID         #
+    #########################
+    bannerColor 'Changing dir to optimized/solid' "blue" "*"
+    cd "${CURRENTDIR}/optimized/solid" || exit
+
+    #  modify file names
+    bannerColor 'Renaming all files in solid dir.' "blue" "*"
+    # in heroicons/solid rename file names 
+    rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/Icon.svelte/' -- *.svg  
+    bannerColor 'Renaming is done.' "green" "*"
+
+    # For each svelte file modify contents of all file by adding
+    bannerColor 'Modifying all files.' "blue" "*"
+
+    # VIEWBOX
+    # viewBox="0 0 20 20" to {viewBox} for solid
+    sed -i 's/viewBox="0 0 20 20"/{viewBox}/' ./*.*
+
+    # Insert script tag at the beginning for solid and insert class={className} and viewBox
+    sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 20 20";<\/script>/' ./*.* && sed -i 's/fill/class={className} &/' ./*.*
+    # END OF VIEWBOX
+
+    bannerColor 'Modification is done in solid dir.' "green" "*"
+
+    bannerColor 'Creating index.js file.' "blue" "*"
+    # list file names to each index.txt
+    find . -type f '(' -name '*.svelte' ')' > index1
+    
+    # removed ./ from each line
+    sed 's/^.\///' index1 > index2
+    rm index1
+
+    # create a names.txt
+    sed 's/.svelte//' index2 > names.txt
+    # Add , after each line in names.txt
+    sed -i 's/$/,/' names.txt
+
+    # Create import section in index2 files.
     # for solid
-    # sed "s:\(.*\)\.svelte:import \1 from './heroicons/solid/&':" index.txt > index-solid.txt
+    sed "s:\(.*\)\.svelte:import \1 from './heroicons/solid/&':" index2 > index3
+    bannerColor 'Created index.js file with import.' "green" "*"
     
 
     #################
@@ -84,50 +143,9 @@ fn_hero() {
 
     bannerColor 'Added export to index.js file.' "green" "*"
 
-    exit
-    ######################### 
-    #         SOLID         #
-    #########################
-    bannerColor 'Changing dir to optimized/solid' "blue" "*"
-    cd "${CURRENTDIR}/optimized/solid" || exit
-
-    #  modify file names
-    bannerColor 'Renaming all files in solid dir.' "blue" "*"
-    # in heroicons/outline and heroicons/solid rename file names 
-    rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/Icon.svelte/' -- *.svg  
-    bannerColor 'Renaming is done.' "green" "*"
-    
-    # For each svelte file modify contents of all file by adding
-    bannerColor 'Modifying all files.' "blue" "*"
-
-    # viewBox="0 0 20 20" to {viewBox} for solid
-    sed -i 's/viewBox="0 0 20 20"/{viewBox}/' ./*.*
-
-    # Insert script tag at the beginning for solid and insert class={className} and viewBox
-    sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 20 20"<\/script>/' ./*.* && sed -i 's/fill/class={className} &/' ./*.*
-
-    bannerColor 'Modification is done in solid dir.' "green" "*"
-
-    # rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/Icon/' -- *.svg  && ls > icon-names.txt
-    
-    #################
-    #    INDEX.JS   #
-    #################
-    
-
-    # Add export{} section
-    # 1 insert export { to index.js, 
-    # 2 insert icon-names to index.js after export { 
-    # 3. append }
-    echo 'export {' >> index-outline.txt && cat index-outline.txt names.txt > index-outline.js && echo '}' >> index-outline.js
-    echo 'export {' >> index-solid.txt && cat index-solid.txt names.txt > index-solid.js && echo '}' >> index-solid.js
-    
-    # copy index.js files to outline and solid dir
-    mv "${CURRENTDIR}/heroicons/index-outline.js" "${CURRENTDIR}/heroicons/outline/index.js" 
-    mv "${CURRENTDIR}/heroicons/index-solid.js" "${CURRENTDIR}/heroicons/solid/index.js"    
     # clean up
     rm icon-names.txt index.txt
     rm -rf "${CURRENTDIR}/heroicons"
     
-    echo "Done."
+    bannerColor 'All done.' "green" "*"
 }
