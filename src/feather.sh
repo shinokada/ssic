03a52eadca7afc9aed2f1d12271e3135ab8b0874
Shinichi Okada <okada.shin@gmail.com>
2022-04-24 09:21:29 +0900

    chore: update feather.sh

--- src/feather.sh
+++ src/feather.sh
@@ -1,109 +1,68 @@
 fn_feather() {
-    ################
-    # This script creates a single directory main 
-    # and it contains all Heroicons.
-    # Move it's contents to Repo svelte-heroicons' lib dir.
-    ######################
-    GITURL="https://github.com/feathericons/feather"
-    # clone heroicons from github
+    ###########################################################
+    # This script creates simple-icons. 
+    ###########################################################
+    GITURL="git@github.com:feathericons/feather.git"
+    DIRNAME='feathericons'
+    ICONDIR='icons'
+    # clone icons from github
     cd "${CURRENTDIR}" || exit 1
-    # if heroicons dir remove it
-    if [ -d "${CURRENTDIR}/feathericons" ]; then
-      bannerColor 'Removing the previous feathericons dir.' "blue" "*"
-      rm -rf "${CURRENTDIR}/feathericons"
+    # if icons dir remove it
+    if [ -d "${CURRENTDIR}/${DIRNAME}" ]; then
+      bannerColor 'Removing the previous dir.' "blue" "*"
+      rm -rf "${CURRENTDIR}/${DIRNAME}"
     fi
 
     # clone it
-    bannerColor 'Cloning feathericons.' "green" "*"
-    git clone "${GITURL}" > /dev/null 2>&1 || {
+    bannerColor 'Cloning the repo.' "green" "*"
+    git clone "${GITURL}" || {
         echo "not able to clone"
         exit 1
     }
 
-    # copy bin from the cloned dir to feathericons dir
-    bannerColor 'Moving feather/bin to the root.' "green" "*"
-    if [ -d "${CURRENTDIR}/bin" ]; then
-      bannerColor 'Removing the previous bin dir.' "blue" "*"
-      rm -rf "${CURRENTDIR}/bin"
+    # copy optimized from the cloned dir to heroicons dir
+    bannerColor 'Moving icons dir to the root.' "green" "*"
+    if [ -d "${CURRENTDIR}/${ICONDIR}" ]; then
+      bannerColor 'Removing the previous icons dir.' "blue" "*"
+      rm -rf "${CURRENTDIR}/${ICONDIR}"
     fi
 
-    mv "${CURRENTDIR}/feathericons/bin" "${CURRENTDIR}"
-
-    # create main dir
-    if [ -d "${CURRENTDIR}/main" ]; then
-      bannerColor 'Removing the previous main dir.' "blue" "*"
-      rm -rf "${CURRENTDIR}/main"
-    fi
-    mkdir "${CURRENTDIR}/main"
-
-    ######################### 
-    #        OUTLINE        #
-    #########################
-    bannerColor 'Changing dir to bin' "blue" "*"
-    cd "${CURRENTDIR}/bin" || exit
+    mv "${CURRENTDIR}/${DIRNAME}/${ICONDIR}" "${CURRENTDIR}"
     exit
+    ######################### 
+    #        ICONS      #
+    #########################
+    bannerColor 'Changing dir to icons dir' "blue" "*"
+    cd "${CURRENTDIR}/${ICONDIR}" || exit
+
+    bannerColor 'Removing all files starting with a number.' "blue" "*"
+    find . -type f -name "[0-9]*"  -exec rm {} \;
+    bannerColor 'Done.' "green" "*"
+    
     #  modify file names
     bannerColor 'Renaming all files in outline dir.' "blue" "*"
     # in heroicons/outline rename file names 
-    rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/IconOutline.svelte/' -- *.svg > /dev/null 2>&1
+    rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/Icon.svelte/' -- *.svg  > /dev/null 2>&1
     bannerColor 'Renaming is done.' "green" "*"
 
-    # For each svelte file modify contents of all file by adding
+    # For each svelte file modify contents of all file
     bannerColor 'Modifying all files.' "blue" "*"
 
     # VIEWBOX
-    # viewBox="0 0 24 24" to {viewBox} for outline
+    # Change from viewBox="0 0 24 24" to {viewBox} for outline
     sed -i 's/viewBox="0 0 24 24"/{viewBox}/' ./*.*
 
     # Insert script tag at the beginning and insert class={className} and viewBox
-    sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 24 24";<\/script>/' ./*.* && sed -i 's/fill/class={className} &/' ./*.*
+    sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 24 24"; export let fill="#000000"<\/script>/' ./*.* && sed -i 's/xmlns/class={className} fill={fill} &/' ./*.*
     # END OF VIEWBOX
 
     bannerColor 'Modification is done in outline dir.' "green" "*"
-
-    # Move all files to main dir
-    mv ./* "${CURRENTDIR}/main"
-
-    
-    ######################### 
-    #         SOLID         #
-    #########################
-
-    bannerColor 'Changing dir to optimized/solid' "blue" "*"
-    cd "${CURRENTDIR}/optimized/solid" || exit
-
-    #  modify file names
-    bannerColor 'Renaming all files in solid dir.' "blue" "*"
-    # in heroicons/solid rename file names 
-    rename -v 's/./\U$&/;s/-(.)/\U$1/g;s/\.svg$/IconSolid.svelte/' -- *.svg > /dev/null 2>&1
-    bannerColor 'Renaming is done.' "green" "*"
-
-    # For each svelte file modify contents of all file by adding
-    bannerColor 'Modifying all files.' "blue" "*"
-
-    # VIEWBOX
-    # viewBox="0 0 20 20" to {viewBox} for solid
-    sed -i 's/viewBox="0 0 20 20"/{viewBox}/' ./*.*
-
-    # Insert script tag at the beginning for solid and insert class={className} and viewBox
-    sed -i '1s/^/<script>export let className="h-6 w-6"; export let viewBox="0 0 20 20";<\/script>/' ./*.* && sed -i 's/fill/class={className} &/' ./*.*
-    # END OF VIEWBOX
-
-    bannerColor 'Modification is done in solid dir.' "green" "*"
-
-    # Move all files to main dir
-    mv ./* "${CURRENTDIR}/main"
-
-    #############################
-    #    INDEX.JS PART 1 IMPORT #
-    #############################
-    cd "${CURRENTDIR}/main" || exit 1
 
     bannerColor 'Creating index.js file.' "blue" "*"
     # list file names to each index.txt
     find . -type f '(' -name '*.svelte' ')' > index1
     
-    # remove ./ from each line
+    # removed ./ from each line
     sed 's/^.\///' index1 > index2
     rm index1
 
@@ -113,14 +72,14 @@ fn_feather() {
     sed -i 's/$/,/' names.txt
 
     # Create import section in index2 files.
-    # for solid
+    # for outline
     sed "s:\(.*\)\.svelte:import \1 from './&':" index2 > index3
     bannerColor 'Created index.js file with import.' "green" "*"
-    
-    ##########################
-    # INDEX.JS PART 2 EXPORT #
-    ##########################
 
+    #################
+    #    INDEX.JS   #
+    #################
+    
     bannerColor 'Adding export to index.js file.' "blue" "*"
     # Add export{} section
     # 1 insert export { to index.js, 
@@ -128,14 +87,13 @@ fn_feather() {
     # 3. append }
     echo 'export {' >> index3 && cat index3 names.txt > index.js && echo '}' >> index.js
 
-    # rm names.txt index2 index3
+    rm names.txt index2 index3
 
     bannerColor 'Added export to index.js file.' "green" "*"
 
+    bannerColor "Cleaning up ${CURRENTDIR}/${DIRNAME}." "blue" "*"
     # clean up
-    rm -rf "${CURRENTDIR}/heroicons"
+    rm -rf "${CURRENTDIR}/${DIRNAME}"
     
     bannerColor 'All done.' "green" "*"
-
-     bannerColor 'Copy all files in the main dir to svelte-heroicons lib directory.' 'magenta' '='
 }
