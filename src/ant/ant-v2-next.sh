d1667fac2d6d55a73c9f38b85702f8548f412e86
Shinichi Okada <147320+shinokada@users.noreply.github.com>
2024-04-29 17:16:25 +0200

    feat: update ant

--- src/ant/ant-v2-next.sh
+++ src/ant/ant-v2-next.sh
@@ -16,28 +16,28 @@ fn_remove(){
   sed -i 's/p-id="[0-9]*"//g' ./*.*
 }
 
-fn_replace_twotone(){
-    if grep -q 'fill="#333"' "${file}"; then
-      # remove fill={color}
-      sed -i 's/fill={color}/\n/' "${filename}"
-      # change export let color="currentColor"; to export let strokeColor="currentColor"
-      # export let strokeColor = ctx.color || "#333";
-      sed -i 's/export let color = ctx.color || "currentColor";/export let strokeColor = ctx.strokeColor || "#333";/' "${filename}"
-      # change fill="#333" to fill={strokeColor}
-      sed -i 's/fill="#333"/fill={strokeColor}/' "${file}"
-    fi
-    if grep -q 'fill="#E6E6E6"' "${file}"; then
-      # change fill="#E6E6E6" to fill={insideColor}
-      sed -i 's/fill="#E6E6E6"/fill={insideColor}/' "${file}"
-    fi
-    # Files with #D9D9D9 fill inside
-    if grep -q 'fill="#D9D9D9"' "${file}"; then
-      # change fill={color} to fill={strokeColor}
-      sed -i 's/fill={color}/fill={strokeColor}/' "${file}"
-      # change fill="#D9D9D9" to fill={fillInside}
-      sed -i 's/fill="#D9D9D9"/fill={insideColor}/' "${file}"
-    fi
-}
+# fn_replace_twotone(){
+#     if grep -q 'fill="#333"' "${file}"; then
+#       # remove fill={color}
+#       sed -i 's/fill={color}/\n/' "${filename}"
+#       # change export let color="currentColor"; to export let strokeColor="currentColor"
+#       # export let strokeColor = ctx.color || "#333";
+#       sed -i 's/export let color = ctx.color || "currentColor";/export let strokeColor = ctx.strokeColor || "#333";/' "${filename}"
+#       # change fill="#333" to fill={strokeColor}
+#       sed -i 's/fill="#333"/fill={strokeColor}/' "${file}"
+#     fi
+#     if grep -q 'fill="#E6E6E6"' "${file}"; then
+#       # change fill="#E6E6E6" to fill={insideColor}
+#       sed -i 's/fill="#E6E6E6"/fill={insideColor}/' "${file}"
+#     fi
+#     # Files with #D9D9D9 fill inside
+#     if grep -q 'fill="#D9D9D9"' "${file}"; then
+#       # change fill={color} to fill={strokeColor}
+#       sed -i 's/fill={color}/fill={strokeColor}/' "${file}"
+#       # change fill="#D9D9D9" to fill={fillInside}
+#       sed -i 's/fill="#D9D9D9"/fill={insideColor}/' "${file}"
+#     fi
+# }
 
 fn_svg_path(){
   for SUBSRC in "${CURRENTDIR}"/*; do
@@ -48,17 +48,28 @@ fn_svg_path(){
     for file in *; do
       FILENAME=$(basename "${file%.*}")
       # create svelte file like address-book-solid.svelte
-      SVETLENAME="${CURRENTDIR}/${FILENAME}-${SUBDIRNAME}.svelte"
+      SVELTENAME="${CURRENTDIR}/${FILENAME}-${SUBDIRNAME}.svelte"
       if [ ! -f "${SUBDIRNAME}/${file}" ]; then
-        cp "${script_dir}/templates/template-v2.txt" "${SVETLENAME}"
+        cp "${script_dir}/templates/template-v2.txt" "${SVELTENAME}"
       fi
 
       SVGPATH=$(extract_svg_path "$file")
       # replace replace_svg_path with svg path
-      sed -i "s;replace_svg_path;${SVGPATH};" "${SVETLENAME}"
+      sed -i "s;replace_svg_path;${SVGPATH};" "${SVELTENAME}"
       # get viewBox value
       VIEWVALUE=$(sed -n 's/.*viewBox="\([^"]*\)".*/\1/p' "${file}")
-      sed -i "s;replace_viewBox;${VIEWVALUE};" "${SVETLENAME}"
+      sed -i "s;replace_viewBox;${VIEWVALUE};" "${SVELTENAME}"
+
+      # replace <defs>    <style\/>    </defs>
+      # sed -i 's|<style/>|<style></style>|' "${SVELTENAME}"
+      sed -i 's|<defs> *<style/> *</defs>||' "${SVELTENAME}"
+      # remove <defs><style type="text/css"></style></defs>
+      sed -i 's|<defs><style type="text/css"></style></defs>||' "${SVELTENAME}"
+      # if SVELTENAME doesn't contain Twotone, remove fill="#333"
+      if [[ ! "$SVELTENAME" =~ "Twotone" ]]; then
+        # Replace only if "Twotone" (case-sensitive) is not found
+        sed -i 's/fill="#333"//' "${SVELTENAME}"
+      fi
     done
   done
 }
@@ -72,7 +83,7 @@ fn_modify_filenames() {
     FILENAME=$(basename "${filename}" .svelte | tr '-' ' ')
     
     # echo "${FILENAME}"
-    sed -i "s;replace_ariaLabel; \"${FILENAME},\" ;" "${filename}" >/dev/null 2>&1
+    sed -i "s;replace_ariaLabel; \"${FILENAME}\" ;" "${filename}" >/dev/null 2>&1
 
     new_name=$(echo "${FILENAMEONE^}")
     # Capitalize the letter after -
@@ -92,7 +103,7 @@ fn_modify_filenames() {
 fn_ant() {
   GITURL="https://github.com/ant-design/ant-design-icons"
   DIRNAME='packages/icons-svg/svg'
-  LOCAL_REPO_NAME="$HOME/Svelte/SVELTE-ICON-FAMILY/svelte-ant-design-icons-next"
+  LOCAL_REPO_NAME="$HOME/Svelte/SVELTE-ICON-FAMILY/svelte-ant-design-icons-next-runes-webkit"
   SVELTE_LIB_DIR='src/lib'
   CURRENTDIR="${LOCAL_REPO_NAME}/${SVELTE_LIB_DIR}"
   
